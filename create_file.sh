63d6c3d5b0dd2aad79234bde61cfec2a10bc668d
Tamryn <tamryn@kineticskunk.com>
2024-08-11 18:50:32 +0200

    Added create_file.sh

---
+++ create_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Name of the new file to be created
+new_file="newly_created_file.txt"
+
+# Create the new file
+touch $new_file
+
+# Check if the file was created successfully
+if [ -f "$new_file" ]; then
+    echo "File '$new_file' created successfully."
+else
+    echo "Failed to create the file '$new_file'."
+fi
